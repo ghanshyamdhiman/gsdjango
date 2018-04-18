1e347137d6c9a199a8bb6dec195dee56d04ebb19
ghanshyamdhiman <ghanshyamdhiman@gmail.com>
2018-04-18 10:50:01 +0530

    Update views.py

--- gsdjango/gsdjango/views.py
+++ gsdjango/gsdjango/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World")
+    return HttpResponse("Hello, World INdex")
+
+def home(request):
+    return HttpResponse("Hello, World HOme")
