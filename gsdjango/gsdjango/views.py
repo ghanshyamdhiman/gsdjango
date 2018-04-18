eabc2fadf26681724e4bf091213f30f95594d3ec
ghanshyamdhiman <ghanshyamdhiman@gmail.com>
2018-04-18 11:03:06 +0530

    Update views.py

--- gsdjango/gsdjango/views.py
+++ gsdjango/gsdjango/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, World INdex")
 
 def home(request):
-    return HttpResponse("Hello, World HOme")
+    return render(request, "templates/index.html", {})
