a101cb974507b27f36c08a9d9aebcd53e2857dd9
ghanshyamdhiman <ghanshyamdhiman@gmail.com>
2018-04-18 12:27:33 +0530

    Update settings.py

--- gsdjango/gsdjango/settings.py
+++ gsdjango/gsdjango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'gsdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'gsdjango/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
