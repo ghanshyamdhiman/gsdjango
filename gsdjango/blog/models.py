497b2b8cb52e4ba5a387c3217ebf59032c15a2c4
ghanshyamdhiman <ghanshyamdhiman@gmail.com>
2018-04-24 23:00:19 +0530

    Update models.py

--- gsdjango/blog/models.py
+++ gsdjango/blog/models.py
@@ -6,7 +6,10 @@ class Blog(models.Model):
     body = models.TextField()
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey('blog.Category')
-
+    
+    def __str__(self):
+        return self.title
+     
 class Category(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
