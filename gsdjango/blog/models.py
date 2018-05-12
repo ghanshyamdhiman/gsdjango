99b91d3975c0cfd429fb04a4b7b0fddd12c8ba36
ghanshyamdhiman <ghanshyamdhiman@gmail.com>
2018-05-12 15:50:45 +0530

    Update models.py

--- gsdjango/blog/models.py
+++ gsdjango/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Blog(model.Model):
+  title = models.CharField(max_length=123)
+  content = models.TextField()
+ 
+  def __str__(self):
+        return self.title
+
 # Create your models here.
