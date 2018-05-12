6b1377a796c787226e1a883237f8ae0e4426aa02
ghanshyamdhiman <ghanshyamdhiman@gmail.com>
2018-05-12 16:12:56 +0530

    Update views.py

--- gsdjango/blog/views.py
+++ gsdjango/blog/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import TemplateView
 
+from . models import Blog
+
 class HomePageView(TemplateView):
+  model = Blog
   template_name = 'home.html'
+  
+  
 
 # Create your views here.
