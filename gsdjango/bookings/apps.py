3caa2eec988a24ac717f948569683978a491c79b
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2019-04-19 21:45:01 +0530

    Update apps.py

--- gsdjango/bookings/apps.py
+++ gsdjango/bookings/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.contrib import admin
+from .models import BookingDetails
 
+admin.site.register(BookingDetails)
 
 class BookingsConfig(AppConfig):
     name = 'bookings'
+
+    
+
+
