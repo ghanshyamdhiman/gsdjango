7372cb63c8991881b7520a27f665be397198e008
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2019-04-19 12:46:35 +0530

    Update models.py

--- gsdjango/bookings/models.py
+++ gsdjango/bookings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class BookingDetails(models.Model):
+    booking_venue = models.CharField(max_length=200)
+    booking_date = models.DateTimeField('Booking Date')
+
+
+class ClientDetails(models.Model):
+    client_name = models.CharField(max_length=200)
+    advance_paid = models.IntegerField(default=0)
 # Create your models here.
