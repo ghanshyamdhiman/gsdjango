a8c6588df77b80cf440b8c2126e557b6a3d530d7
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2019-04-19 22:02:34 +0530

    Update views.py

--- gsdjango/bookings/views.py
+++ gsdjango/bookings/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the bookings index.")
 
+def booking(request, booking_venue):
+    return HttpResponse("You're looking at booking venue %s." % booking_venue)
+
+
 # Create your views here.
