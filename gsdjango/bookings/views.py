da839a7443e5d448b889aaf2c7291bda6ebe03ef
Ghanshyam Dhiman <ghanshyamdhiman@gmail.com>
2019-04-20 22:10:45 +0530

    Update views.py

--- gsdjango/bookings/views.py
+++ gsdjango/bookings/views.py
@@ -4,11 +4,10 @@ from .models import BookingDetails
 
 def index(request):
     latest_booking_list = BookingDetails.objects.order_by('booking_date')[:5]
-    output = ', '.join([q.booking_venue for q in latest_booking_list])
-    return HttpResponse(output)
-
-def booking(request, booking_venue):
-    return HttpResponse("You're looking at booking venue %s." % booking_venue)
-
+    template = loader.get_template('bookings/index.html')
+    context = {
+        'latest_booking_list': latest_booking_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
